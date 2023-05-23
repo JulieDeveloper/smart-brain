6f6bb9cef34228fd8dbf5cd3620b5a3b8b5fcc1e
Julie Chou <chouyuan424@gmail.com>
2023-05-23 15:55:32 -0400

    feat: add Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,33 @@
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <p
+          onClick={() => onRouteChange('signOut')}
+          className="f3 link fim black undefine pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <p
+          onClick={() => onRouteChange('signIn')}
+          className="f3 link fim black undefine pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRouteChange('register')}
+          className="f3 link fim black undefine pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
+};
+
+export default Navigation;
