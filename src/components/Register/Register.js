b23c3f9fb15ac54917e527c6b987a2860f9a2a26
Julie Chou <chouyuan424@gmail.com>
2023-05-25 07:03:26 -0400

    feat: Connect register request to backend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,41 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 const Register = ({ onRouteChange }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: '',
+  });
+  const loadUser = (data) => {
+    setUser({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined,
+    });
+  };
+
+  const handleSubmitRegister = () => {
+    axios
+      .post('http://localhost:3001/register', {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        loadUser(response.data);
+        onRouteChange('home');
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-1 mw6 shadow-5 center">
       <main className="pa4 black-80">
@@ -14,6 +51,8 @@ const Register = ({ onRouteChange }) => {
                 type="text"
                 name="name"
                 id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className="mt3">
@@ -25,6 +64,8 @@ const Register = ({ onRouteChange }) => {
                 type="email"
                 name="email-address"
                 id="email-address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="mv3">
@@ -36,12 +77,14 @@ const Register = ({ onRouteChange }) => {
                 type="password"
                 name="password"
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </fieldset>
           <div className="">
             <input
-              onClick={() => onRouteChange('home')}
+              onClick={handleSubmitRegister}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Register"
